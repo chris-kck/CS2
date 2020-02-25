d879fc21dac8b88a8089f0a56904f0825344f21c
Kudzai Kateera <ktrkud001@sl-dual-196.cs.uct.ac.za>
2020-02-25 15:53:24 +0200

    Add data to array

--- src/LSArrayApp.java
+++ src/LSArrayApp.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class LSArrayApp {
     public static void main(String[] args) {
         try {
-            File myObj = new File("Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt");
+            File myObj = new File("./src/Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt");
             Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
+	    int counter=0;
+            String[] ArrayData= new String[2976];
+	    while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
+		
+		ArrayData[counter++]=data;
                 System.out.println(data);
             }
+
+            System.out.println("The element is "+ArrayData[2970]);
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
