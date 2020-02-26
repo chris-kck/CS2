8148bb82eff7ed62dc58c7512cf0a55d9b9a471c
Kudzai Kateera <ktrkud001@myuct.ac.za>
2020-02-26 14:20:25 +0200

    Output matching areas

--- src/LSArrayApp.java
+++ src/LSArrayApp.java
@@ -12,48 +12,52 @@ public class LSArrayApp {
     static String[] ArrayData= new String[2976];
 
     public static void main(String[] args) {
+        //read file and add to list
         try {
             File myObj = new File("./src/Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt");
             Scanner myReader = new Scanner(myObj);
 	        int counter=0;
+
 	    while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
         		ArrayData[counter++]=data;
-                System.out.println(data);
+                //System.out.println(data);
             }
-
-            System.out.println("The element is "+ArrayData[2970]);
-            String[] splited = ArrayData[2970].split(" ");
-	        System.out.println("splitted is:" + splited[0]);
+            //System.out.println("The element is "+ArrayData[2970]);
+            //String[] splited = ArrayData[2970].split(" ");
+	        //System.out.println("splitted is:" + splited[0]);
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
-        System.out.println("Hello KC");
+        //System.out.println("Hello KC");
 
-        if (args.length == 0){
-            System.out.println("Length"+args.length);
+        if (args.length > 0){
+            System.out.println("Number of arguments: "+args.length);
         printAreas(args[0], args[1], args[2]);
         }
+        else {System.out.println("No arguments passed");}
     }
     public static void printAreas (String stage, String day, String startTime){
         String concat = stage + "_" +  day + "_" + startTime;
-        System.out.println("Test");
+        System.out.println("print Areas called");
 
 
         for (String data: ArrayData) {
-            String[] data0 = data.split(" "); //data element0
-            if (data0[0].equals(concat)){
-                System.out.println("Matching areas: " + data );
+            String[] splitted = data.split(" "); //data element0 2 elements: date etc, areas
+            if (splitted[0].equals(concat)){
+                System.out.println("Matching areas: " + splitted[1] );
                 //.equals
                 //add matching areas to array which will be later output.
             }
 
         }
     }
-    public static void printAllAreas (){
+    public static void printAllAreas (
+
+    ){
 
     }
 }
