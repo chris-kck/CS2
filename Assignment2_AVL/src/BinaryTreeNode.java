1ab920dfd0d8dd3c52e9f2d69a306ddc0c05d039
Chris Kay <kckateera@gmail.com>
2020-04-29 16:00:05 +0200

    Update BinaryTreeNode with height variable

--- Assignment2_AVL/src/BinaryTreeNode.java
+++ Assignment2_AVL/src/BinaryTreeNode.java
@@ -7,12 +7,14 @@ public class BinaryTreeNode<String>
    String data;
    BinaryTreeNode<String> left;
    BinaryTreeNode<String> right;
+   int height;
    
    public BinaryTreeNode ( String d, BinaryTreeNode<String> l, BinaryTreeNode<String> r )
    {
       data = d;
       left = l;
       right = r;
+      height = 0;
    }
    
    BinaryTreeNode<String> getLeft () { return left; }
