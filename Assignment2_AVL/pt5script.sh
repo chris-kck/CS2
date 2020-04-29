d31e5bde1b4af2fc569a503e722518d30e122399
Chris Kay <kckateera@gmail.com>
2020-04-29 23:52:56 +0200

    Final Script to be run

--- Assignment2_AVL/pt5script.sh
+++ Assignment2_AVL/pt5script.sh
@@ -133,4 +133,126 @@ cat src/subset/2900.txt | while read line; do
  make avl "params=$1" 2>> src/subset/AVLfind2900.txt
 done
 
+cat src/subset/300.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind300.txt
+done
+
+
+cat src/subset/600.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind600.txt
+done
+
+cat src/subset/900.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind900.txt
+done
+
+cat src/subset/1200.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind1200.txt
+done
+
+cat src/subset/1500.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind1500.txt
+done
+
+cat src/subset/1800.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind1800.txt
+done
+
+cat src/subset/2100.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind2100.txt
+done
+
+cat src/subset/2400.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind2400.txt
+done
+
+cat src/subset/2700.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind2700.txt
+done
+
+cat src/subset/2900.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/BSTfind2900.txt
+done
+
+cat src/subset/300.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind300.txt
+done
+
+
+cat src/subset/600.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind600.txt
+done
+
+cat src/subset/900.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind900.txt
+done
+
+cat src/subset/1200.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind1200.txt
+done
+
+cat src/subset/1500.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind1500.txt
+done
+
+cat src/subset/1800.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind1800.txt
+done
+
+cat src/subset/2100.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind2100.txt
+done
+
+cat src/subset/2400.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind2400.txt
+done
+
+cat src/subset/2700.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind2700.txt
+done
+
+cat src/subset/2900.txt | while read line; do
+ set $line
+ #echo $1
+ make bst "params=$1" 2>> src/subset/AVLfind2900.txt
+done
+
 unset ifs
--- Assignment2_AVL/test.sh
+++ Assignment2_AVL/test.sh
@@ -2,7 +2,7 @@ IFS=" "
 cat src/subset/300.txt | while read line; do
  set $line
  #echo $1
- make avl "params=$1" 2>> insertCount/BSTfind300.txt
+ make avl "params=$1" 2>> src/subset/BSTfind300.txt
 done
 
 unset IFS
