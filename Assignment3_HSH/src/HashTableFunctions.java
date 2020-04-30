e1ac314740b90db07ed6896cbbb94f34ee50deae
Chris Kay <kckateera@gmail.com>
2020-04-30 14:46:52 +0200

    Ass 3 Initial Commit

---
+++ Assignment3_HSH/src/HashTableFunctions.java
@@ -0,0 +1,33 @@
+class HashTableFunctions
+{
+   int hashTableSize;
+   String [] hashTableArray;
+   
+   // constructor
+   public HashTableFunctions ( int size, String [] array )
+   {
+      hashTableSize = size;
+      hashTableArray = array;
+   }
+
+   // hash function
+   public int hash ( String s )
+   {
+      int sum = 0;
+      
+      // WRITE YOUR CODE HERE
+
+      return sum;
+   }   
+   
+   // return True if the hash table contains string s
+   // return False if the hash table does not contain string s
+   boolean find ( String s )
+   {
+      boolean found = false;
+      
+      // WRITE YOUR CODE HERE
+      
+      return found;
+   }
+}
