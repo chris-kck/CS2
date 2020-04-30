354033b39fd0e7de14629b7c62993dcf5951eafb
Chris Kay <kckateera@gmail.com>
2020-04-30 14:19:31 +0200

    Upload pdf reports

--- Assignment2_AVL/pt5avl.sh
+++ Assignment2_AVL/pt5avl.sh
@@ -72,3 +72,25 @@ cat src/subset/2900.txt | while read line; do
  make avl "params=$1" 2>> src/subset/AVLfind2900.txt
 done
 unset ifs
+0: commit 4eb34b12447a733644b866dbd76d221310625730
+1: Author: root <root@li345-229.members.linode.com>
+2: Date: Thu Apr 30 08:51:39 2020 +0200
+3:
+4: Accurate AVL Data
+5:
+6: commit e5f04f5cff13a3a72596a03b7a74dd7564764eb5
+7: Author: Chris Kay <kckateera@gmail.com>
+8: Date: Thu Apr 30 07:18:51 2020 +0200
+9:
+...
+163: commit b18002cdd4a324e475a9547e9f467c6e07eb5234
+164: Author: Chris Kay <kckateera@gmail.com>
+165: Date: Wed Apr 29 15:41:12 2020 +0200
+166:
+167: Packages renaming
+168:
+169: commit 3a28d52c531ae8f20325a2d98989c378aa6e03fb
+170: Author: Chris Kay <kckateera@gmail.com>
+171: Date: Wed Apr 29 15:29:33 2020 +0200
+172:
+
